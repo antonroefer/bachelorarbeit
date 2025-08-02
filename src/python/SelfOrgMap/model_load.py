4983e5368614aa7d659a8fbf9c42f1f445402494
antonroefer <anton.roefer@web.de>
2025-08-02 14:31:34 +0200

    ml 08

--- src/python/SelfOrgMap/model_load.py
+++ src/python/SelfOrgMap/model_load.py
@@ -239,7 +239,7 @@ print(f"Anzahl der erkannten Merkmale: {num_features}")
 for i in range(data.shape[1]):
     data[:, i] = min_max_scale(data[:, i])
 
-apx = "07"
+apx = "08"
 
 # Definiere den Pfad zum gespeicherten Modell
 model_path = os.path.join(script_dir, "Runs", f"R{apx}", f"trained_som_{apx}.pkl")
