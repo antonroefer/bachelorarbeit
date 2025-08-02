7b7d1e891afc4bd93b1aa275e778a2ff642d53c3
antonroefer <anton.roefer@web.de>
2025-08-02 11:56:35 +0200

    ready for model_load on nesh

--- src/python/SelfOrgMap/model_load.py
+++ src/python/SelfOrgMap/model_load.py
@@ -139,10 +139,13 @@ with h5py.File(t_path, "r") as f:
 
 ratio = x.max() / t.max() * (9 / 16)
 
+"""
+################################################################################################################################################
+"""
 
 # Eigene Daten laden
 script_dir = os.path.dirname(os.path.abspath(__file__))
-data_file = "feature_vectors_khigh.npz"
+data_file = "feature_vectors.npz"
 data_path = os.path.join(script_dir, data_file)
 with np.load(data_path) as npzfile:
     # Gib die Namen der Arrays in der .npz-Datei aus
@@ -152,11 +155,7 @@ with np.load(data_path) as npzfile:
 
     # --- NEU: Feature-Auswahl ---
     # Liste der gewünschten Features
-    desired_features = [
-        "quadrature",
-        "inst_freq_raw",
-        "kurtosis",
-    ]
+    desired_features = ["inst_amp", "inst_freq_raw", "semblance", "kurtosis", "dip"]
 
     # Annahme: Die Namen der Features sind in der .npz-Datei unter dem Schlüssel 'feature_names' gespeichert
     if "feature_names" in npzfile.files:
@@ -240,7 +239,7 @@ print(f"Anzahl der erkannten Merkmale: {num_features}")
 for i in range(data.shape[1]):
     data[:, i] = min_max_scale(data[:, i])
 
-apx = "41"
+apx = "09"
 
 # Definiere den Pfad zum gespeicherten Modell
 model_path = os.path.join(script_dir, "Runs", f"R{apx}", f"trained_som_{apx}.pkl")
@@ -250,7 +249,7 @@ name = f"{apx}_full_on_rg_{i_rg}" if not cut else apx
 
 # Den Plot erstellen
 save_plots = True  # Setze auf True, um die Plots zu speichern
-cmap = ColorMap2DTeuling2
+cmap = ColorMap2DZiegler
 
 som.plot_u_matrix(save=save_plots, appendix=apx, name=name)  # U-Matrix Plot
 # som.plot_som_neighbor_distances(
