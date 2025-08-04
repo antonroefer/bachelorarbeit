14c6571510f8e05ccbdc9de80b9cf15fa80a554f
antonroefer <anton.roefer@web.de>
2025-08-04 10:35:55 +0200

    load 52

--- src/python/SelfOrgMap/model_load.py
+++ src/python/SelfOrgMap/model_load.py
@@ -145,7 +145,7 @@ ratio = x.max() / t.max() * (9 / 16)
 
 # Eigene Daten laden
 script_dir = os.path.dirname(os.path.abspath(__file__))
-data_file = "feature_vectors_khigh.npz"
+data_file = "feature_vectors_khigh_AE.npz"
 data_path = os.path.join(script_dir, data_file)
 with np.load(data_path) as npzfile:
     # Gib die Namen der Arrays in der .npz-Datei aus
@@ -157,7 +157,7 @@ with np.load(data_path) as npzfile:
     # Liste der gewünschten Features
     desired_features = [
         "quadrature",
-        #"inst_freq_raw",
+        "inst_freq_raw",
         "kurtosis",
     ]
 
@@ -243,7 +243,7 @@ print(f"Anzahl der erkannten Merkmale: {num_features}")
 for i in range(data.shape[1]):
     data[:, i] = min_max_scale(data[:, i])
 
-apx = "51"
+apx = "52"
 
 # Definiere den Pfad zum gespeicherten Modell
 model_path = os.path.join(script_dir, "Runs", f"R{apx}", f"trained_som_{apx}.pkl")
